e90b03bc96c070842876818eb21ccf3b44c84548
tucker19 <adam.dhowell.19@gmail.com>
2024-04-23 14:01:20 -0500

    update to query

--- astro/plugins/fleetio_utils/sql/still_playing_with_this.sql
+++ astro/plugins/fleetio_utils/sql/still_playing_with_this.sql
@@ -1,4 +1,7 @@
-select date_trunc('day', updated_at) as updated_at_day, state, count(*) as updated
+select date_trunc('day', updated_at) as updated_at_day,
+  sum(case when state = 'open' then 1 else 0 end) as opened,
+  sum(case when state = 'closed' then 1 else 0 end) as closed,
+  sum(case when state = 'resolved' then 1 else 0 end) as resolved
 from issues
-group by updated_at_day, state
+group by updated_at_day
 order by updated_at_day;
